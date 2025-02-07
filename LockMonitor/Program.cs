fbc32f18e69eb0878e91cd1f4da6d7afb67a641d
avokado03 <graure@yandex.ru>
2025-02-07 14:42:12 +0500

    Monitor, Mutex

--- LockMonitor/Program.cs
+++ LockMonitor/Program.cs
@@ -2,9 +2,75 @@
 {
 	internal class Program
 	{
+		static object locker = new object();
+
+		static int value = 0;
+
 		static void Main(string[] args)
 		{
-			Console.WriteLine("Hello, World!");
+
+			for (int i = 0; i < 4; i++)
+			{
+				var thread1 = new Thread(Increment4);
+				thread1.Name = $"Thread [{i}]";
+				thread1.Start();
+			}
+
+			Console.ReadKey();
+		}
+
+		//Race conditions without lock
+		static void Increment()
+		{
+			lock (locker)
+			{
+				for (int i = 0; i < 10; i++)
+				{
+					Console.WriteLine($"{Thread.CurrentThread.Name} - {i.ToString()} - {value}");
+					++value;
+				}
+			}
+		}
+
+		// equals lock
+		static void Increment2()
+		{
+			bool acquiredLock = false;
+			try
+			{
+				Monitor.Enter(locker, ref acquiredLock);
+				for (int i = 0; i < 10; i++)
+				{
+					Console.WriteLine($"{Thread.CurrentThread.Name} - {i.ToString()} - {value}");
+					++value;
+				}
+			}
+			finally
+			{
+				if(acquiredLock) 
+					Monitor.Exit(locker);
+			}
+		}
+
+		// without lock 1
+		static void Increment3()
+		{
+			for (int i = 0; i < 10; i++)
+			{
+				Console.WriteLine($"{Thread.CurrentThread.Name} - {i.ToString()} - {value}");
+				++value;
+			}
+		}
+
+		// without lock 2
+		static void Increment4()
+		{
+			int x = 1;
+			for (int i = 1; i < 6; i++)
+			{
+				Console.WriteLine($"{Thread.CurrentThread.Name} - {i.ToString()} - {x}");
+				x++;
+			}
 		}
 	}
 }
---
+++ Mutex/Program.cs
@@ -0,0 +1,42 @@
+﻿namespace MutexExample
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			bool firstInstance;
+			Mutex mutex = new Mutex(false, "my_named_mutex", out firstInstance);
+			if (!firstInstance)
+			{
+				Console.WriteLine("Уже запущена копия приложения!");
+				Console.ReadKey();
+			}
+			else
+			{
+				int x;
+				Mutex local_mutex = new Mutex();
+				object locker = new object();
+				for (int i = 1; i < 6; i++)
+				{
+					Thread thread = new Thread(new ThreadStart(Func));
+					thread.Name = i.ToString();
+					thread.Start();
+				}
+				Console.ReadKey();
+				void Func()
+				{
+					local_mutex.WaitOne();
+					x = 0;
+					for (int i = 0; i < 10; i++)
+					{
+						x++;
+						Console.WriteLine($"Поток {Thread.CurrentThread.Name} вывел число {x}");
+						Thread.Sleep(100);
+					}
+					local_mutex.ReleaseMutex();
+				}
+			}
+			GC.KeepAlive(mutex);
+		}
+	}
+}
