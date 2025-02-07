4576138a0bcbe42c79033bbf98073be1b9e09ec7
avokado03 <graure@yandex.ru>
2025-02-07 15:05:07 +0500

    Semaphore

---
+++ Semaphore_SemaphoreSlim/Program.cs
@@ -0,0 +1,6 @@
+﻿using Semaphore_SemaphoreSlim;
+
+for (int i = 1; i < 7; i++)
+{
+	Visitor visitor = new Visitor(i);
+}
---
+++ Semaphore_SemaphoreSlim/Visitor.cs
@@ -0,0 +1,34 @@
+﻿namespace Semaphore_SemaphoreSlim
+{
+	internal class Visitor
+	{
+		//Здесь вся функциональность сосредоточена в классе Visitor. 
+		//Так как семафор должны видеть все потоки, то поле _semaphore объявлено как static.
+
+		/// <summary>
+		/// The SemaphoreSlim is a lightweight alternative to the Semaphore class that doesn't use Windows kernel semaphores. 
+		/// Unlike the Semaphore class, the SemaphoreSlim class doesn't support named system semaphores. 
+		/// You can use it as a local semaphore only. 
+		/// The SemaphoreSlim class is the recommended semaphore for synchronization within a single app.
+		/// </summary>
+		static Semaphore _semaphore = new Semaphore(3, 3);
+		private readonly Thread myThread;
+		public Visitor(int i)
+		{
+			myThread = new Thread(Fun)
+			{
+				Name = $"Посетитель #{i}"
+			};
+			myThread.Start();
+		}
+		public static void Fun()
+		{
+			_semaphore.WaitOne();
+			Console.WriteLine($"{Thread.CurrentThread.Name} входит в клуб");
+			Console.WriteLine($"{Thread.CurrentThread.Name} веселится");
+			Thread.Sleep(1000);
+			Console.WriteLine($"{Thread.CurrentThread.Name} покидает клуб");
+			_semaphore.Release();
+		}
+	}
+}
