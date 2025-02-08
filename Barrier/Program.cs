ecf74163c14c40cb4b552113c2d8465429f851db
avokado03 <graure@yandex.ru>
2025-02-09 04:42:48 +0500

    Barrier

---
+++ Barrier/Program.cs
@@ -0,0 +1,25 @@
+﻿namespace BarrierExample
+{
+	// <href>https://www.albahari.com/threading/part4.aspx</href>
+	internal class Program
+	{
+		// with post-phase action
+		static Barrier _barrier = new Barrier (3, barrier => Console.WriteLine());
+
+		static void Main()
+		{
+			new Thread(Speak).Start();
+			new Thread(Speak).Start();
+			new Thread(Speak).Start();
+		}
+
+		static void Speak()
+		{
+			for (int i = 0; i < 5; i++)
+			{
+				Console.Write(i + " ");
+				_barrier.SignalAndWait();
+			}
+		}
+	}
+}
