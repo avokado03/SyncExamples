60e44cd6241c7a247d36f572091781af3c7e59c4
avokado03 <graure@yandex.ru>
2025-02-10 17:58:22 +0500

    Exchange

--- Interlocked/Program.cs
+++ Interlocked/Program.cs
@@ -4,9 +4,12 @@
 	{
 		static void Main(string[] args)
 		{
+			Console.WriteLine("--Increment--");
 			WithoutInterlocked();
 			Console.WriteLine();
 			WithInterlocked();
+			Console.WriteLine("\n --Exchange--");
+			Exchange();
 			Console.ReadLine();
 		}
 
@@ -32,6 +35,28 @@
 			});
 			Console.WriteLine("Expected Result: 100000");
 			Console.WriteLine($"Actual Result Interlocked: {IncrementValue}");
+		}
+
+		static long x = 0;
+		static void Exchange()
+		{
+			Thread thread1 = new Thread(new ThreadStart(SomeMethod));
+			thread1.Start();
+			thread1.Join();
+			// Written [20]
+			Console.WriteLine("Exchange 20: {0}",Interlocked.Exchange(ref x, 20));
+			Console.ReadKey();
+		}
+
+		static void SomeMethod()
+		{
+			// Replace x with 20.
+			Interlocked.Exchange(ref x, 20);
+			// CompareExchange: if x is 20, then change to current DateTime.Now.Day or any integer variable.
+			//long result = Interlocked.CompareExchange(ref Program.x, DateTime.Now.Day, 20);
+			long result = Interlocked.CompareExchange(ref x, 50, 20);
+			// Returns original value from CompareExchange
+			Console.WriteLine("Exchange 50: {0}", result);
 		}
 	}
 }
