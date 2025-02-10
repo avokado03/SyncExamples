1238332cb197ceb8c65cfa8c8afe035770e1775a
avokado03 <graure@yandex.ru>
2025-02-10 17:36:14 +0500

    Interlocked

---
+++ Interlocked/Program.cs
@@ -0,0 +1,37 @@
+﻿namespace InterlockedExample
+{
+	internal class Program
+	{
+		static void Main(string[] args)
+		{
+			WithoutInterlocked();
+			Console.WriteLine();
+			WithInterlocked();
+			Console.ReadLine();
+		}
+
+		static void WithoutInterlocked()
+		{
+			var IncrementValue = 0;
+			Parallel.For(0, 100000, _ =>
+			{
+				//Incrementing the value
+				IncrementValue++;
+			});
+			Console.WriteLine("Expected Result: 100000");
+			Console.WriteLine($"Actual Result: {IncrementValue}");
+		}
+
+		static void WithInterlocked()
+		{
+			var IncrementValue = 0;
+			Parallel.For(0, 100000, _ =>
+			{
+				//Incrementing the value
+				Interlocked.Increment(ref IncrementValue);
+			});
+			Console.WriteLine("Expected Result: 100000");
+			Console.WriteLine($"Actual Result Interlocked: {IncrementValue}");
+		}
+	}
+}
